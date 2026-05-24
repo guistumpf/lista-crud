e7fa4b531634f7207d9338a1fa801d0f9bbbe72c
João Guilherme Stumpf Fernandes <guistumpf@users.noreply.github.com>
2026-05-24 14:07:27 -0300

    crud basico com a lista de tarefas

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,116 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { useState } from "react"
+import "./globals.css"
+
+export default function List() {
+
+  const [input, setinput] = useState("")
+  const [tarefas, settarefas] = useState<string[]>([])
+  const [inputedit, setedit] = useState<string>("")
+  const [editindex, setindex] = useState(null)
+
+  function add() {
+
+    if (input === "") {
+      alert("Digite algo")
+      return
+    }
+
+    settarefas([...tarefas, input])
+    setinput("")
+  }
+
+  function edit(index: any) {
+    setedit(tarefas[index])
+    setindex(index)
+
+  }
+
+  function confirmedit() {
+
+    settarefas(tarefas.map((tarefa, i) => {
+      return i == editindex ? inputedit : tarefa
+    }))
+    setindex(null)
+
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="min-h-screen bg-white flex justify-center ">
+      <div className="bg-white p-6 rounded-xl w-full max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Lista de Tarefas
+        </h1>
+        {/* Input */}
+        <div className="flex gap-2 mb-6">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setinput(e.target.value)}
+            className="flex-1 border rounded-lg px-3 py-2 outline-none"
+            placeholder="Nova tarefa..."
+          />
+          <button
+            onClick={add}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 cursor-pointer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Add
+          </button>
         </div>
-      </main>
+
+        {/* Tasks */}
+        <ul className="space-y-3">
+          {tarefas.map((tarefa, index) => (
+            <li
+              key={index}
+              className="bg-gray-100 p-3  flex justify-between items-center"
+            >
+              {index === editindex ? (
+                <div className="flex gap-2 w-full">
+                  <input
+                    type="text"
+                    value={inputedit}
+                    onChange={(e) => setedit(e.target.value)}
+                    className="flex-1 border rounded px-2 py-1"
+                  />
+
+                  <button
+                    onClick={() => confirmedit()}
+                    className="bg-green-500 text-white px-3 rounded cursor-pointer"
+                  >
+                    Confirm
+                  </button>
+                </div>
+              ) : (
+                <>
+                  <span>{tarefa}</span>
+
+                  <div className="flex gap-2">
+                    <button
+                      onClick={() => edit(index)}
+                      className="bg-purple-500 text-white px-3 py-1 rounded cursor-pointer"
+                    >
+                      🖉
+                    </button>
+
+                    <button
+                      onClick={() =>
+                        settarefas(
+                          tarefas.filter((a, i) => i !== index)
+                        )
+                      }
+                      className="bg-red-500 text-white px-3 py-1 rounded cursor-pointer"
+                    >
+                      X
+                    </button>
+                  </div>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
